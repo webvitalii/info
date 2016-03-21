4e00579c916ed895a3b7491611daa1e04e7eae85
Vitalii <vitalii.skorokhod@gmail.com>
2016-03-20 22:51:19 -0600

    Ver.2.4
    Code cleanup and refactoring;

--- info.php
+++ info.php
@@ -3,51 +3,57 @@
 Plugin Name: info
 Plugin URI: http://wordpress.org/plugins/info/
 Description: Plugin shows in the admin bar the number of SQL queries, the amount of time in seconds and memory load.
-Version: 2.3
+Version: 2.4
 Author: webvitaly
 Author URI: http://web-profile.com.ua/wordpress/plugins/
 License: GPLv3
 */
 
 
-if ( !function_exists( 'info_unqprfx_admin_bar' ) ) {
+if ( !function_exists( 'info_plugin_admin_bar' ) ) {
 
-	function info_unqprfx_admin_bar() { // add info to admin bar
-		global $wp_admin_bar, $wpdb;
-		if ( !is_super_admin() || !is_admin_bar_showing() )
+	function info_plugin_admin_bar() {
+		global $wp_admin_bar;
+		if ( !is_super_admin() || !is_admin_bar_showing() ) {
 			return;
-		//$useful_info = get_num_queries().' sql q.; '.timer_stop().' sec.;';
-		$useful_info = sprintf( '%d q; %.2f sec; %.2f MB;',
+		}
+		$useful_info = sprintf(
+			'%d q; %.2f sec; %.2f MB;',
 			get_num_queries(),
 			timer_stop( 0, 3 ),
 			memory_get_peak_usage() / 1024 / 1024
 		);
-		/* Add the main siteadmin menu item */
 		$wp_admin_bar->add_menu( array( 'id' => 'useful_info', 'title' => $useful_info, 'href' => FALSE ) );
 		//$wp_admin_bar->add_menu( array( 'parent' => 'useful_info', 'title' => $useful_info, 'href' => FALSE ) );
 	}
-	add_action( 'admin_bar_menu', 'info_unqprfx_admin_bar', 1000 );
+	add_action( 'admin_bar_menu', 'info_plugin_admin_bar', 1000 );
 
 
-	function info_unqprfx_code($atts, $content = null) { // add info to source code
-		$useful_info = sprintf( '%d q; %.2f sec; %.2f MB;',
+	function info_plugin_code($atts, $content = null) {
+		$useful_info = sprintf(
+			'%d q; %.2f sec; %.2f MB;',
 			get_num_queries(),
 			timer_stop( 0, 3 ),
 			memory_get_peak_usage() / 1024 / 1024
 		);
-		echo "\n".'<!-- info plugin v.2.3 wordpress.org/plugins/info/ -->'."\n";
-		echo '<!-- ========== '.$useful_info.' ========== -->'."\n";
+		echo "\n".'<!-- Debug info: '.$useful_info.' -->'."\n";
+		echo '<!-- info plugin v.2.3 wordpress.org/plugins/info/ -->'."\n";
 	}
-	add_action('wp_footer', 'info_unqprfx_code');
+	add_action('wp_footer', 'info_plugin_code');
 
 
-	function info_unqprfx_plugin_meta( $links, $file ) { // add 'Support' and 'Donate' links to plugin meta row
-		if ( strpos( $file, 'info.php' ) !== false ) {
-			$links = array_merge( $links, array( '<a href="http://web-profile.com.ua/wordpress/plugins/info/" title="Plugin page">info</a>' ) );
-			$links = array_merge( $links, array( '<a href="http://web-profile.com.ua/donate/" title="Support the development">donate</a>' ) );
+	function info_plugin_plugin_meta( $links, $file ) {
+		if ( $file == plugin_basename( __FILE__ ) ) {
+			$row_meta = array(
+				'support' => '<a href="http://web-profile.com.ua/wordpress/plugins/info/" target="_blank"><span class="dashicons dashicons-editor-help"></span> ' . __( 'info', 'info' ) . '</a>',
+				'donate' => '<a href="http://web-profile.com.ua/donate/" target="_blank"><span class="dashicons dashicons-heart"></span> ' . __( 'Donate', 'info' ) . '</a>',
+				'pro' => '<a href="http://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii" target="_blank" title="Speedup and protect WordPress in a smart way"><span class="dashicons dashicons-star-filled"></span> ' . __( 'Silver Bullet Pro', 'info' ) . '</a>'
+			);
+			$links = array_merge( $links, $row_meta );
 		}
-		return $links;
+		return (array) $links;
+		
 	}
-	add_filter( 'plugin_row_meta', 'info_unqprfx_plugin_meta', 10, 2 );
+	add_filter( 'plugin_row_meta', 'info_plugin_plugin_meta', 10, 2 );
 
 }
--- readme.txt
+++ readme.txt
@@ -1,19 +1,20 @@
 === info ===
 Contributors: webvitaly
 Donate link: http://web-profile.com.ua/donate/
-Tags: info, sql, queries, query, time, memory
+Tags: info, sql, queries, query, time, memory, debug
 Requires at least: 3.0
-Tested up to: 4.1
-Stable tag: 2.3
+Tested up to: 5.0
+Stable tag: 2.4
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.html
 
 Plugin shows in the admin bar the number of SQL queries, the amount of time in seconds and memory load.
 
 == Description ==
-> [WordPress Pro plugins](http://codecanyon.net/popular_item/by_category?category=wordpress&ref=webvitaly) |
-> [Info plugin](http://web-profile.com.ua/wordpress/plugins/info/ "Plugin page") |
-> [Donate](http://web-profile.com.ua/donate/ "Support the development")
+> **[Silver Bullet Pro](http://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii "Speedup and protect WordPress in a smart way")** |
+> **[Info plugin](http://web-profile.com.ua/wordpress/plugins/info/ "Info plugin page")** |
+> **[Donate](http://web-profile.com.ua/donate/ "Support the development")** |
+> **[GitHub](https://github.com/webvitalii/info "Fork")**
 
 Plugin shows in the admin bar:
 * the number of SQL queries during the WordPress execution
@@ -24,9 +25,8 @@ Admin bar can be enabled in your profile: `"Users => Your Profile => Show Admin 
 This information also added to the bottom of the source code as HTML comment.
 
 = Useful: =
-* ["Anti-spam" - block spam in comments](http://wordpress.org/plugins/anti-spam/ "no spam, no captcha")
-* ["Page-list" - show list of pages with shortcodes](http://wordpress.org/plugins/page-list/ "list of pages with shortcodes")
-* [WordPress Pro plugins](http://codecanyon.net/popular_item/by_category?category=wordpress&ref=webvitaly)
+* **[Silver Bullet Pro - Speedup and protect WordPress in a smart way](http://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii "Speedup and protect WordPress in a smart way")**
+* **[Anti-spam Pro - Block spam in comments](http://codecanyon.net/item/antispam-pro/6491169?ref=webvitalii "Block spam in comments")**
 
 == Screenshots ==
 
@@ -34,6 +34,9 @@ This information also added to the bottom of the source code as HTML comment.
 2. info in the source code
 
 == Changelog ==
+
+= 2.4 - 2016-03-20 =
+* code cleanup and refactoring
 
 = 2.3 - 2012-11-05 =
 * minor changes
